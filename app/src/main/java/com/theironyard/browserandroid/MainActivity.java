b878560d04e525968b786537d1024820697a5f00
Steven Burris <stevenburris.jr@gmail.com>
2016-09-28 11:35:56 -0400

    Created layout

--- app/src/main/java/com/theironyard/browserandroid/MainActivity.java
+++ app/src/main/java/com/theironyard/browserandroid/MainActivity.java
@@ -2,12 +2,25 @@ package com.theironyard.browserandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    WebView web;
+    Button back;
+    Button forward;
+    EditText addressBar;
+    Button go;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        back = (Button) findViewById(R.id.back);
+        web = (WebView) findViewById(R.id.web);
+        forward = (Button) findViewById(R.id.forward);
     }
 }
